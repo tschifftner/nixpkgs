583444acd41e9c19b8ba92aa791e16780582a0a3
Tobias Schifftner <ts@ambimax.de>
2023-12-05 11:42:17 +0100

    init

---
+++ darwin/homebrew.nix
@@ -0,0 +1,112 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkIf elem;
+  caskPresent = cask: lib.any (x: x.name == cask) config.homebrew.casks;
+  brewEnabled = config.homebrew.enable;
+  homePackages = config.home-manager.users.${config.users.primaryUser.username}.home.packages;
+in
+
+{
+  environment.shellInit = mkIf brewEnabled ''
+    eval "$(${config.homebrew.brewPrefix}/brew shellenv)"
+  '';
+
+  # https://docs.brew.sh/Shell-Completion#configuring-completions-in-fish
+  # For some reason if the Fish completions are added at the end of `fish_complete_path` they don't
+  # seem to work, but they do work if added at the start.
+  programs.fish.interactiveShellInit = mkIf brewEnabled ''
+    if test -d (brew --prefix)"/share/fish/completions"
+      set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
+
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+      set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
+  '';
+
+  homebrew.enable = true;
+  homebrew.onActivation.autoUpdate = true;
+  homebrew.onActivation.cleanup = "zap";
+  homebrew.global.brewfile = true;
+
+  homebrew.taps = [
+    "homebrew/cask"
+    "homebrew/cask-drivers"
+    "homebrew/cask-fonts"
+    "homebrew/cask-versions"
+    "homebrew/core"
+    "homebrew/services"
+    "nrlquaker/createzap"
+  ];
+
+  # Prefer installing application from the Mac App Store
+  homebrew.masApps = {
+    "1Password for Safari" = 1569813296;
+    DaisyDisk = 411643860;
+    "Dark Mode for Safari" = 1397180934;
+    Deliveries = 290986013;
+    Keynote = 409183694;
+    "Notion Web Clipper" = 1559269364;
+    Numbers = 409203825;
+    Pages = 409201541;
+    "Save to Raindrop.io" = 1549370672;
+    Slack = 803453959;
+    "Tailscale" = 1475387142;
+    "Things 3" = 904280696;
+    Xcode = 497799835;
+  };
+
+  # If an app isn't available in the Mac App Store, or the version in the App Store has
+  # limitiations, e.g., Transmit, install the Homebrew Cask.
+  homebrew.casks = [
+    "1password"
+    "1password-cli"
+    "anki"
+    "balenaetcher"
+    "clay"
+    "etrecheckpro"
+    "firefox"
+    "google-chrome"
+    "google-drive"
+    # "hammerspoon"
+    "keybase"
+    "ngrok"
+    "pdf-expert"
+    "postman"
+    "raindropio"
+    "raycast"
+    "transmit"
+    "visual-studio-code"
+    "helm"
+    "devtoys"
+    "moneymoney"
+    "obsidian"
+    "whatsapp"
+  ];
+
+  # Configuration related to casks
+  home-manager.users.${config.users.primaryUser.username} =
+    mkIf (caskPresent "1password-cli" && config ? home-manager) {
+      programs.ssh.enable = true;
+      programs.ssh.extraConfig = ''
+        # Only set `IdentityAgent` not connected remotely via SSH.
+        # This allows using agent forwarding when connecting remotely.
+        Match host * exec "test -z $SSH_TTY"
+          IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+      '';
+      home.shellAliases = {
+        cahix = mkIf (elem pkgs.cachix homePackages) "op plugin run -- cachix";
+        gh = mkIf (elem pkgs.gh homePackages) "op plugin run -- gh";
+        nixpkgs-review = mkIf (elem pkgs.nixpkgs-review homePackages) "op run -- nixpkgs-review";
+      };
+      home.sessionVariables = {
+        GITHUB_TOKEN = "op://Private/github.com/Sicherheit/Personal Access Token";
+      };
+    };
+
+  # For cli packages that aren't currently available for macOS in `nixpkgs`.Packages should be
+  # installed in `../home/default.nix` whenever possible.
+  homebrew.brews = [
+  ];
+}
---
+++ flake.nix
@@ -0,0 +1,235 @@
+{
+  description = "Tobias` Nix system configs, and some other useful stuff.";
+
+  inputs = {
+    # Package sets
+    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    # Environment/system management
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+
+    # Flake utilities
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # Utility for watching macOS `defaults`.
+    prefmanager.url = "github:malob/prefmanager";
+    prefmanager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    prefmanager.inputs.flake-compat.follows = "flake-compat";
+    prefmanager.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { self, darwin, home-manager, flake-utils, ... }@inputs:
+    let
+      inherit (self.lib) attrValues makeOverridable mkForce optionalAttrs singleton;
+
+      homeStateVersion = "23.11";
+
+      nixpkgsDefaults = {
+        config = {
+          allowUnfree = true;
+        };
+        overlays = attrValues self.overlays ++ [
+          inputs.prefmanager.overlays.prefmanager
+        ] ++ singleton (
+          final: prev: (optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+            # Sub in x86 version of packages that don't build on Apple Silicon.
+            inherit (final.pkgs-x86)
+              # agda
+              # idris2
+              ;
+          }) // {
+            # Add other overlays here if needed.
+          }
+        );
+      };
+
+      primaryUserDefaults = {
+        username = "ts";
+        fullName = "Tobias Schifftner";
+        email = "tobias@schifftner.de";
+        nixConfigDirectory = "/Users/ts/.config/nixpkgs";
+      };
+    in
+    {
+      # Add some additional functions to `lib`.
+      lib = inputs.nixpkgs-unstable.lib.extend (_: _: {
+        mkDarwinSystem = import ./lib/mkDarwinSystem.nix inputs;
+        lsnix = import ./lib/lsnix.nix;
+      });
+
+      # Overlays --------------------------------------------------------------------------------{{{
+
+      overlays = {
+        # Overlays to add different versions `nixpkgs` into package set
+        pkgs-master = _: prev: {
+          pkgs-master = import inputs.nixpkgs-master {
+            inherit (prev.stdenv) system;
+            inherit (nixpkgsDefaults) config;
+          };
+        };
+        pkgs-stable = _: prev: {
+          pkgs-stable = import inputs.nixpkgs-stable {
+            inherit (prev.stdenv) system;
+            inherit (nixpkgsDefaults) config;
+          };
+        };
+        pkgs-unstable = _: prev: {
+          pkgs-unstable = import inputs.nixpkgs-unstable {
+            inherit (prev.stdenv) system;
+            inherit (nixpkgsDefaults) config;
+          };
+        };
+        apple-silicon = _: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+          # Add access to x86 packages system is running Apple Silicon
+          pkgs-x86 = import inputs.nixpkgs-unstable {
+            system = "x86_64-darwin";
+            inherit (nixpkgsDefaults) config;
+          };
+        };
+        tweaks = _: _: {
+          # Add temporary overrides here
+        };
+      };
+      # }}}
+
+      # Modules -------------------------------------------------------------------------------- {{{
+
+      darwinModules = {
+        # My configurations
+       bootstrap = import ./darwin/bootstrap.nix;
+       defaults = import ./darwin/defaults.nix;
+       general = import ./darwin/general.nix;
+       homebrew = import ./darwin/homebrew.nix;
+
+        # Local modules
+        users-primaryUser = import ./modules/darwin/users.nix;
+      };
+
+      homeManagerModules = {
+        # My configurations
+       colors = import ./home/colors.nix;
+       config-files = import ./home/config-files.nix;
+       fish = import ./home/fish.nix;
+       git = import ./home/git.nix;
+       git-aliases = import ./home/git-aliases.nix;
+       gh-aliases = import ./home/gh-aliases.nix;
+       kitty = import ./home/kitty.nix;
+       neovim = import ./home/neovim.nix;
+       packages = import ./home/packages.nix;
+       starship = import ./home/starship.nix;
+       starship-symbols = import ./home/starship-symbols.nix;
+
+        # Modules I've created
+        colors = import ./modules/home/colors;
+        programs-neovim-extras = import ./modules/home/programs/neovim/extras.nix;
+        programs-kitty-extras = import ./modules/home/programs/kitty/extras.nix;
+        home-user-info = { lib, ... }: {
+          options.home.user-info =
+            (self.darwinModules.users-primaryUser { inherit lib; }).options.users.primaryUser;
+        };
+      };
+      # }}}
+
+      # System configurations ------------------------------------------------------------------ {{{
+
+      darwinConfigurations = {
+        # Minimal macOS configurations to bootstrap systems
+        bootstrap-x86 = makeOverridable darwin.lib.darwinSystem {
+          system = "x86_64-darwin";
+          modules = [ ./darwin/bootstrap.nix { nixpkgs = nixpkgsDefaults; } ];
+        };
+        bootstrap-arm = self.darwinConfigurations.bootstrap-x86.override {
+          system = "aarch64-darwin";
+        };
+
+        # My Apple Silicon macOS laptop config
+        TobiasMacbookPro = makeOverridable self.lib.mkDarwinSystem (primaryUserDefaults // {
+          modules = attrValues self.darwinModules ++ singleton {
+            nixpkgs = nixpkgsDefaults;
+            networking.computerName = "Tobias` 💻";
+            networking.hostName = "TobiasMacbookPro";
+            networking.knownNetworkServices = [
+              "Wi-Fi"
+              "USB 10/100/1000 LAN"
+            ];
+            nix.registry.my.flake = inputs.self;
+          };
+          extraModules = singleton {
+            nix.linux-builder.enable = true;
+            nix.linux-builder.maxJobs = 8;
+            nix.linux-builder.config = {
+              virtualisation.darwin-builder.memorySize = 16 * 1024;
+              virtualisation.cores = 8;
+            };
+          };
+          inherit homeStateVersion;
+          homeModules = attrValues self.homeManagerModules;
+        });
+
+        # Config with small modifications needed/desired for CI with GitHub workflow
+        githubCI = self.darwinConfigurations.TobiasMacbookPro.override {
+          system = "x86_64-darwin";
+          username = "runner";
+          nixConfigDirectory = "/Users/runner/work/nixpkgs/nixpkgs";
+          extraModules = singleton {
+            environment.etc.shells.enable = mkForce false;
+            environment.etc."nix/nix.conf".enable = mkForce false;
+            homebrew.enable = mkForce false;
+          };
+        };
+      };
+
+      # Config I use with non-NixOS Linux systems (e.g., cloud VMs etc.)
+      # Build and activate on new system with:
+      # `nix build .#homeConfigurations.ts.activationPackage && ./result/activate`
+      homeConfigurations.ts = makeOverridable home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs-unstable (nixpkgsDefaults // { system = "x86_64-linux"; });
+        modules = attrValues self.homeManagerModules ++ singleton ({ config, ... }: {
+          home.username = config.home.user-info.username;
+          home.homeDirectory = "/home/${config.home.username}";
+          home.stateVersion = homeStateVersion;
+          home.user-info = primaryUserDefaults // {
+            nixConfigDirectory = "${config.home.homeDirectory}/.config/nixpkgs";
+          };
+        });
+      };
+
+      # Config with small modifications needed/desired for CI with GitHub workflow
+      homeConfigurations.runner = self.homeConfigurations.ts.override (old: {
+        modules = old.modules ++ singleton {
+          home.username = mkForce "runner";
+          home.homeDirectory = mkForce "/home/runner";
+          home.user-info.nixConfigDirectory = mkForce "/home/runner/work/nixpkgs/nixpkgs";
+        };
+      });
+      # }}}
+
+    } // flake-utils.lib.eachDefaultSystem (system: {
+      # Re-export `nixpkgs-unstable` with overlays.
+      # This is handy in combination with setting `nix.registry.my.flake = inputs.self`.
+      # Allows doing things like `nix run my#prefmanager -- watch --all`
+      legacyPackages = import inputs.nixpkgs-unstable (nixpkgsDefaults // { inherit system; });
+
+      # Development shells ----------------------------------------------------------------------{{{
+      # Shell environments for development
+      # With `nix.registry.my.flake = inputs.self`, development shells can be created by running,
+      # e.g., `nix develop my#python`.
+      devShells = let pkgs = self.legacyPackages.${system}; in
+        {
+          python = pkgs.mkShell {
+            name = "python310";
+            inputsFrom = attrValues {
+              inherit (pkgs.pkgs-master.python310Packages) black isort;
+              inherit (pkgs) poetry python310 pyright;
+            };
+          };
+        };
+      # }}}
+    });
+}
