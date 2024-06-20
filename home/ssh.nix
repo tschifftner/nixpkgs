02b26bd6ac6028ddd8bc300972692e8aa8d1f370
Tobias Schifftner <tobias@schifftner.de>
2024-06-20 10:26:47 +0200

    feat: updated

--- home/ssh.nix
+++ home/ssh.nix
@@ -7,7 +7,7 @@ let
   ambimaxPubKey = readFile ./ssh/ambimax-hetzner-cloud.pub;
   githubPubKey = readFile ./ssh/github.pub;
   rotaryPubKey = readFile ./ssh/ambimax-rotary.pub;
-  tsVpnKey = readFile ./ssh/ts-vpn.key;
+  tsVpnKey = readFile ./ssh/ts-vpn.pub;
 in {
   programs.ssh.extraConfig = "Include config.d/*";
 
@@ -15,13 +15,15 @@ in {
     # Common settings for all hosts
     Host karm ts-k3s-arm ts-k3s nixos ambimax-intern ambimax-intern-k8s-lb monitoring
         User root
-        IdentityFile ${writeText "ambimax-hetzner-cloud.key" ambimaxPubKey}
+        IdentityFile ${writeText "ambimax-hetzner-cloud.pub" ambimaxPubKey}
         IdentitiesOnly yes
 
     # Specific host settings
     Host ts-vpn ts-proxy
+        User root
         HostName 49.12.75.12
-        IdentityFile ${writeText "ts-vpn.key" tsVpnKey}
+        IdentityFile ${writeText "ts-vpn.pub" tsVpnKey}
+        IdentitiesOnly yes
 
     Host github.com
         HostName github.com
